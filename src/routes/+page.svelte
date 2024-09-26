4f602f65bd60e5dae914a63fc000046c0d8bcc40
LordDeatHunter <desuhansa@gmail.com>
2024-09-26 22:56:40 +0200

    Disable overscroll

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 <main id="home" class="bg-home flex flex-col items-center justify-center gap-0">
   <div class="grow-in flex flex-col items-center justify-center gap-4">
     <p class="fade-in text-5xl font-[960]">DeatHunter</p>
-    <p class="slower-fade-in text-[2rem]">[Software Engineer]</p>
+    <p class="slower-fade-in text-[2rem] text-center">[Software Engineer]</p>
     <img src="dh.png" class="mb-6 mt-8 h-auto w-40" alt="DeatHunter Avatar" />
     <div class="flex flex-col items-center justify-center gap-3">
       <p class="fade-in m-0 max-w-md text-center text-xl font-normal">
