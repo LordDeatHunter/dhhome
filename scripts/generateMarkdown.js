7e5dfa7c4662d32f393b951edaa02dff5c4dbf1a
LordDeatHunter <desuhansa@gmail.com>
2024-10-06 04:25:44 +0200

    Add synthax highlighting for Markdown files

--- scripts/generateMarkdown.js
+++ scripts/generateMarkdown.js
@@ -1,9 +1,21 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
-import { marked } from 'marked';
+import { Marked } from 'marked';
+import { markedHighlight } from 'marked-highlight';
+import hljs from 'highlight.js';
 
 const BASE_PATH = path.resolve('static/data');
+
+const marked = new Marked(
+  markedHighlight({
+    langPrefix: 'hljs language-',
+    highlight(code, lang) {
+      const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+      return hljs.highlight(code, { language }).value;
+    }
+  })
+);
 
 const markdownFilter = (file) => {
   return typeof file === 'string' ? file.endsWith('.md') : false;
@@ -17,7 +29,8 @@ const projectCategories = {};
 projectCategoriesFiles.forEach((file) => {
   const fileContent = fs.readFileSync(path.join(projectCategoriesDir, file), 'utf-8');
   const { data, content } = matter(fileContent);
-  const htmlContent = marked(content);
+
+  const htmlContent = marked.parse(content);
 
   const projectCategoryType = file.replace('.md', '');
 
