cec35e0ee9b3557293ab89c6a51b724d47a4fbd9
LordDeatHunter <desuhansa@gmail.com>
2025-01-31 00:40:35 +0100

    Add bottlecap map

--- src/routes/hobbies/beercaps/+page.server.ts
+++ src/routes/hobbies/beercaps/+page.server.ts
@@ -1,6 +1,7 @@
 import caps from '$lib/caps.json';
+import map from '$lib/world.geo.json';
 import type { Bottlecaps } from '$lib/types';
 
 export async function load() {
-  return caps as Bottlecaps;
+  return { caps: caps as Bottlecaps, map: map as GeoJSON.GeoJSON };
 }
--- src/routes/hobbies/beercaps/+page.svelte
+++ src/routes/hobbies/beercaps/+page.svelte
@@ -5,9 +5,13 @@
   import Bottlecap from '$components/Bottlecap.svelte';
   import type { Bottlecap as BottlecapType } from '$lib/types';
   import ImageModal from '$components/ImageModal.svelte';
+  import { FeatureState, FillLayer, GeoJSONSource, LineLayer, MapLibre } from 'svelte-maplibre-gl';
 
   interface Props {
-    data: Bottlecaps;
+    data: {
+      caps: Bottlecaps;
+      map: GeoJSON.GeoJSON;
+    };
   }
 
   let { data }: Props = $props();
@@ -18,7 +22,7 @@
   let searchWords = $derived(search.trim().toLowerCase().split(' '));
 
   const sortedBottlecaps = $derived.by<Array<BottlecapType>>(() => {
-    const filteredBottlecaps = Object.values(data).filter((bottlecap) => {
+    const filteredBottlecaps = Object.values(data.caps).filter((bottlecap) => {
       const bottlecapWords = `${bottlecap.name} ${bottlecap.country}`.toLowerCase();
       return searchWords.every((word) => bottlecapWords.includes(word));
     });
@@ -29,13 +33,43 @@
         : a.country.localeCompare(b.country) || a.name.localeCompare(b.name)
     );
   });
+
+  let hoveredId: string | number | undefined = $state();
 </script>
 
 <main id="hobbies" class="bg-hobbies flex flex-col items-center gap-0">
   <div class="page-content">
-    <h1 class="fade-in text-7xl font-[960] text-center">Hobbies - Beer Bottlecap Collection</h1>
+    <h1 class="fade-in text-center text-7xl font-[960]">Hobbies - Beer Bottlecap Collection</h1>
     <NavBar />
     <div class="divider-h slower-fade-in"></div>
+    <MapLibre
+      class="maplibre-visited"
+      style="https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json"
+      zoom={3.5}
+      center={{ lng: 16, lat: 45 }}
+      attributionControl={false}
+    >
+      <GeoJSONSource data={data.map}>
+        <FillLayer
+          paint={{
+            'fill-color': ['get', 'color'],
+            'fill-opacity': ['case', ['boolean', ['feature-state', 'hover'], false], 0.4, 0.1]
+          }}
+          onmousemove={(ev) => (hoveredId = ev.features?.[0]?.id)}
+          onmouseleave={() => (hoveredId = undefined)}
+        />
+        <LineLayer
+          paint={{
+            'line-color': ['get', 'color'],
+            'line-opacity': ['case', ['boolean', ['feature-state', 'hover'], false], 1, 0.3],
+            'line-width': 1
+          }}
+        />
+        {#if hoveredId}
+          <FeatureState id={hoveredId} state={{ hover: true }} />
+        {/if}
+      </GeoJSONSource>
+    </MapLibre>
     <input
       type="text"
       placeholder="Search bottlecaps..."
@@ -57,7 +91,7 @@
       </button>
     </div>
     <h3 class="slower-fade-in text-center text-3xl font-[960]">
-      Showing {sortedBottlecaps.length} / {Object.keys(data).length} bottlecaps.
+      Showing {sortedBottlecaps.length} / {Object.keys(data.caps).length} bottlecaps.
     </h3>
     <div class="flex justify-center gap-4">
       <div class="slower-fade-in flex max-w-[1440px] flex-col gap-8">
