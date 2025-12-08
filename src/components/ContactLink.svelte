7c054d56a5336de3fe221495ac135331182f87fa
LordDeatHunter <desuhansa@gmail.com>
2025-12-08 21:12:11 +0100

    Update contact page

--- src/components/ContactLink.svelte
+++ src/components/ContactLink.svelte
@@ -7,15 +7,10 @@
     action: () => void;
     actionIcon: string;
     actionText: string;
+    animationDelay?: string;
   }
 
-  let {
-    text,
-    mainIcon,
-    action,
-    actionIcon,
-    actionText
-  }: Props = $props();
+  let { text, mainIcon, action, actionIcon, actionText, animationDelay }: Props = $props();
 
   const handleKeyDown = (event: KeyboardEvent) => {
     if (event.code === 'Enter' || event.code === 'Space') {
@@ -24,10 +19,15 @@
   };
 </script>
 
-<div class="contact-link">
+<div
+  class="contact-link slide-up"
+  style="animation-delay: {animationDelay ? animationDelay : '0s'};"
+>
   <div class="contact-main">
-    <Icon icon={mainIcon} class="h-8 w-8" />
-    <h2 class="text-2xl font-[960]">{text}</h2>
+    <div class="contact-icon">
+      <Icon icon={mainIcon} class="h-8 w-8" />
+    </div>
+    <h2 class="contact-text text-xl font-[960] sm:text-2xl">{text}</h2>
   </div>
   <div
     onclick={action}
@@ -37,7 +37,7 @@
     tabindex="0"
     role="button"
   >
-    <Icon icon={actionIcon} class="h-6 w-6" />
-    {actionText}
+    <Icon icon={actionIcon} class="h-5 w-5" />
+    <span class="contact-action-text">{actionText}</span>
   </div>
 </div>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -15,47 +15,57 @@
 <main id="contact" class="bg-contact flex flex-col items-center gap-0 overflow-x-hidden">
   <div class="page-content">
     <h1 class="fade-in text-4xl font-[960] sm:text-5xl md:text-6xl lg:text-7xl">Contact</h1>
+    <p class="fade-in text-center text-lg opacity-80 sm:text-xl">
+      Platforms where I roam around on.
+    </p>
     <NavBar />
   </div>
-  <div class="contact-links">
-    <ContactLink
-      text="Discord Contact"
-      mainIcon="simple-icons:discord"
-      action={() => copyToClipboard('lorddeathunter')}
-      actionIcon="pixelarticons:copy"
-      actionText={copied ? 'Copied!' : 'Copy'}
-    />
-    <div class="divider-h-dark"></div>
-    <ContactLink
-      text="Personal Discord Server"
-      mainIcon="simple-icons:discord"
-      action={() => window.open('https://discord.gg/vcQEH4x6z2')}
-      actionIcon="pixelarticons:external-link"
-      actionText="Join"
-    />
-    <div class="divider-h-dark"></div>
-    <ContactLink
-      text="GitHub"
-      mainIcon="simple-icons:github"
-      action={() => window.open('https://github.com/lorddeathunter')}
-      actionIcon="pixelarticons:external-link"
-      actionText="Open"
-    />
-    <div class="divider-h-dark"></div>
-    <ContactLink
-      text="CurseForge"
-      mainIcon="simple-icons:curseforge"
-      action={() => window.open('https://www.curseforge.com/members/lorddeathunter/projects')}
-      actionIcon="pixelarticons:external-link"
-      actionText="Open"
-    />
-    <div class="divider-h-dark"></div>
-    <ContactLink
-      text="Itch.io"
-      mainIcon="simple-icons:itchdotio"
-      action={() => window.open('https://lorddeathunter.itch.io/')}
-      actionIcon="pixelarticons:external-link"
-      actionText="Open"
-    />
+  <div class="contact-links-container">
+    <div class="contact-links">
+      <ContactLink
+        animationDelay="0.1s"
+        text="Discord Contact"
+        mainIcon="simple-icons:discord"
+        action={() => copyToClipboard('lorddeathunter')}
+        actionIcon="pixelarticons:copy"
+        actionText={copied ? 'Copied!' : 'Copy'}
+      />
+      <div class="divider-h-dark"></div>
+      <ContactLink
+        animationDelay="0.2s"
+        text="Personal Discord Server"
+        mainIcon="simple-icons:discord"
+        action={() => window.open('https://discord.gg/vcQEH4x6z2')}
+        actionIcon="pixelarticons:external-link"
+        actionText="Join"
+      />
+      <div class="divider-h-dark"></div>
+      <ContactLink
+        animationDelay="0.3s"
+        text="GitHub"
+        mainIcon="simple-icons:github"
+        action={() => window.open('https://github.com/lorddeathunter')}
+        actionIcon="pixelarticons:external-link"
+        actionText="Open"
+      />
+      <div class="divider-h-dark"></div>
+      <ContactLink
+        animationDelay="0.4s"
+        text="CurseForge"
+        mainIcon="simple-icons:curseforge"
+        action={() => window.open('https://www.curseforge.com/members/lorddeathunter/projects')}
+        actionIcon="pixelarticons:external-link"
+        actionText="Open"
+      />
+      <div class="divider-h-dark"></div>
+      <ContactLink
+        animationDelay="0.5s"
+        text="Itch.io"
+        mainIcon="simple-icons:itchdotio"
+        action={() => window.open('https://lorddeathunter.itch.io/')}
+        actionIcon="pixelarticons:external-link"
+        actionText="Open"
+      />
+    </div>
   </div>
 </main>
