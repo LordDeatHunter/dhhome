9dcc0bf4403acb953060469b466be6af41d26c4c
LordDeatHunter <desuhansa@gmail.com>
2025-12-12 03:14:35 +0100

    Center page titles

--- src/components/PageHeader.svelte
+++ src/components/PageHeader.svelte
@@ -10,7 +10,9 @@
 </script>
 
 <div class="page-content">
-  <h1 class="header-title text-4xl font-[960] sm:text-5xl md:text-6xl lg:text-7xl">{title}</h1>
+  <h1 class="header-title text-center text-4xl font-[960] sm:text-5xl md:text-6xl lg:text-7xl">
+    {title}
+  </h1>
   <p class="header-subtitle text-center text-lg opacity-80 sm:text-xl">{subtitle}</p>
   <NavBar />
 </div>
