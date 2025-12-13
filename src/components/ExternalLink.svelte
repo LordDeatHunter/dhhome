3c515b3c2f89096e1389a2a385fb3563c3d3cc58
LordDeatHunter <desuhansa@gmail.com>
2025-12-13 03:16:37 +0100

    Improve button styling

--- src/components/ExternalLink.svelte
+++ src/components/ExternalLink.svelte
@@ -8,15 +8,17 @@
   }
 
   let { url, icon = 'pixelarticons:external-link', children }: Props = $props();
+
+  const iconOnly = !children;
 </script>
 
 <a
   href={url}
   target="_blank"
   rel="noopener noreferrer"
-  class="button flex w-fit items-center gap-1"
+  class="button flex w-fit items-center gap-1 {iconOnly ? 'icon-only' : ''}"
   onclick={(e) => e.stopPropagation()}
 >
   {@render children?.()}
-  <Icon {icon} class="h-8 w-8" />
+  <Icon {icon} class={iconOnly ? 'h-6 w-6' : 'h-8 w-8'} />
 </a>
